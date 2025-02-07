c438260dae90997c9d26e72fb960528837334c01
Armando Rios <armandorios.dev@gmail.com>
2025-02-06 19:09:48 -0800

    feat: connect bot to discord

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const connectDB = require("./db")
+const runBot = require('./src/discord/main.js')
 const app = express()
 const PORT = 3000
 
 connectDB()
+runBot()
 
 app.use(express.json())
 
---
+++ src/discord/main.js
@@ -0,0 +1,15 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+})
+
+
+function runBot() {
+  client.once('ready', () => {
+    console.log(`Bot connected as ${client.user.tag}`)
+  })
+  client.login(process.env.DISCORD_TOKEN)
+}
+
+module.exports = runBot
