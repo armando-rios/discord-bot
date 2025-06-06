117a5be4a09cf8566a7e8b6c1043c8c3155755df
Armando Rios <armandorios.dev@gmail.com>
2025-06-06 12:17:05 -0700

    feat: use new client method (loadEvents)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { GatewayIntentBits } from "discord.js";
 import ExtendedClient from "./estructures/ExtendedClient.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 // Configuracion del cliente
 const client = new ExtendedClient({
@@ -10,7 +12,10 @@ const client = new ExtendedClient({
   ],
 })
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 // Cargar comandos y eventos
+client.loadEvents(path.join(__dirname, "events"));
 
 // Iniciar la aplicacion
 client.login(process.env.TOKEN)
