5c4f454d582dc605295d559a1078e891b1265aa1
Armando Rios <armandorios.dev@gmail.com>
2025-06-06 12:13:05 -0700

    feat(estructures): add loadEvents method on ExtendClient

--- src/estructures/ExtendedClient.js
+++ src/estructures/ExtendedClient.js
@@ -1,4 +1,6 @@
 import { Client, Collection } from 'discord.js';
+import fs from 'fs';
+import path from 'path';
 
 class ExtendedClient extends Client {
   constructor(options) {
@@ -6,6 +8,26 @@ class ExtendedClient extends Client {
     this.commands = new Collection();
     this.events = new Collection();
     this.config = {};
+  }
+
+  // Método para cargar eventos
+  loadEvents(eventsPath) {
+    const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+    for (const file of eventFiles) {
+      const eventPath = path.join(eventsPath, file);
+
+      import(eventPath).then(event => {
+        if (event.default && event.default.name && typeof event.default.execute === 'function') {
+          this.events.set(event.default.name, event.default.execute);
+          this.on(event.default.name, (...args) => event.default.execute(this, ...args));
+        } else {
+          console.warn(`El archivo ${file} no tiene un evento válido.`);
+        }
+      }).catch(err => {
+        console.error(`Error al cargar el evento ${file}:`, err);
+      });
+    }
   }
 }
 
