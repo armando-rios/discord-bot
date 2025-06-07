e9c63ea3d939ff96513aa35c0cdc03bf8d05a3d0
Armando Rios <armandorios.dev@gmail.com>
2025-06-07 08:52:05 -0700

    feat(estructures): add loadCommands to ExtendedClient

--- src/estructures/ExtendedClient.js
+++ src/estructures/ExtendedClient.js
@@ -29,6 +29,35 @@ class ExtendedClient extends Client {
       });
     }
   }
+
+  // Método para cargar comandos
+  loadCommands(commandsPath) {
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+    for (const file of commandFiles) {
+      const commandPath = path.join(commandsPath, file);
+      import(commandPath).then(command => {
+        if (command.default.data && command.default.execute) this.commands.set(command.default.data.name, command.default)
+      }).catch(err => {
+        console.error(`Error al cargar el comando ${file}:`, err);
+      });
+    }
+
+    this.on('interactionCreate', async (interaction) => {
+      if (!interaction.isCommand()) return;
+
+      const command = this.commands.get(interaction.commandName);
+      if (!command) return;
+
+      try {
+        await command.execute(interaction);
+      } catch (error) {
+        console.error(`Error al ejecutar el comando ${interaction.commandName}:`, error);
+        await interaction.reply({ content: 'Hubo un error al ejecutar este comando.', ephemeral: true });
+      }
+    })
+  }
+
 }
 
 export default ExtendedClient;
